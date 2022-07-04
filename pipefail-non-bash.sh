37f5c515f8677a3e8115bd797024ef541db86f12
Shakiba Moshiri <net@shakiba.net>
2022-07-04 20:30:49 +0430

    Add bash strict mode to pipefail-non-bash.sh file

--- pipefail-non-bash.sh
+++ pipefail-non-bash.sh
@@ -1,5 +1,15 @@
 #!/bin/dash
 
+################################################################################
+# bash strict mode
+################################################################################
+set -T # inherit DEBUG and RETURN trap for functions
+set -C # prevent file overwrite by > &> <>
+set -E # inherit -e
+set -e # exit immediately on errors
+set -u # exit on not assigned variables
+set -o pipefail # exit on pipe failure
+
 filename=${1:?'firstname is needed'};
 
 ls -la $filename  | { [  $? -eq 0 ] && grep -z shm; case $? in 0 ) echo done;; * ) echo error;; esac; }
